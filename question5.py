4412ad23a57b8ca94bfb126a37a2650e634677f4
JancoYs <jancoyspeert@gmail.com>
2014-04-07 10:05:03 +0200

    Create question5.py

---
+++ question5.py
@@ -0,0 +1,18 @@
+DnaString = raw_input("Please input a DNA string: ")
+
+nn = 0
+counter = 0
+while counter < len(DnaString):
+    if DnaString[counter] in "ACGTacgt":
+	counter += 1
+    else:
+	counter += 1
+	nn += 1
+
+if nn > 0:
+  print "Please enter a DNA string containing only A, C, G, T and a, c, g, t"
+else:
+  reverse = DnaString[::-1]
+  reverse = reverse.replace("t", "U")
+  reverse = reverse.replace("T", "U")
+  print "The Reverse with U's is :", reverse
